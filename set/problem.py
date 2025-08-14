b7408dc446a26adbae5d47415e8a8b13cd1ea62a
anagha-04 <anaghanilkumar04@gmail.com>
2025-08-14 08:36:54 +0530

    set

---
+++ set/problem.py
@@ -0,0 +1,20 @@
+
+
+st1 = {10,20,30,40}
+
+st2 = {100,10,200,20,300}
+
+#common elements
+
+intersection_set = st1.intersection(st2)
+
+
+
+print(intersection_set)
+
+
+#union
+
+union_set = st1.union(st2)
+
+print(union_set)
